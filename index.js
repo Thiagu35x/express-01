a5d2b1f99d85514dda9dd8dc37c5c85dc470a4ca
Thiago Castro <thiagocastro799@gmail.com>
2026-04-17 18:29:51 +0000

    Ajuste final: caminho das rotas e nome do modulo

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 const express = require('express');
 const app = express();
 
-// ESTA LINHA ABAIXO DEVE ESTAR FALTANDO OU ERRADA:
-const tarefaRoutes = require('./src/routes/tarefaRoutes'); 
+// Puxando o arquivo de rotas que está dentro de api/routes/
+const tarefaRoutes = require('./api/routes/tarefa'); 
 
 app.use(express.json());
 
-// Aqui é onde o erro acontece porque 'tarefaRoutes' não foi definido lá em cima
+// Ativando as rotas de tarefas
 app.use('/tarefas', tarefaRoutes); 
 
 module.exports = app;
