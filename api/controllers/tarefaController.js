12d38e2e774d5fe0073a4479e8c9b98a07850482
Thiago Castro <thiagocastro799@gmail.com>
2026-04-17 19:45:03 +0000

    Agora vai: Tudo salvo e conectado

--- api/controllers/tarefaController.js
+++ api/controllers/tarefaController.js
@@ -1,16 +1,14 @@
 const Tarefa = require('../models/tarefa');
 
-// Buscar todas as tarefas
 exports.getAll = async (req, res) => {
   try {
     const tarefas = await Tarefa.findAll();
-    res.status(200).json(tarefas);
+    res.json(tarefas);
   } catch (error) {
     res.status(500).json({ error: error.message });
   }
 };
 
-// Criar uma nova tarefa
 exports.create = async (req, res) => {
   try {
     const novaTarefa = await Tarefa.create(req.body);
