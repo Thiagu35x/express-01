a2e5469a78dd2e2d15241225f11a27549398069b
Thiago Castro <thiagocastro799@gmail.com>
2026-04-17 18:42:04 +0000

    Adicionando lógica ao tarefaController

--- api/controllers/tarefaController.js
+++ api/controllers/tarefaController.js
@@ -1,10 +1,27 @@
-const express = require('express');
-const router = express.Router();
-// Importa o controlador que você acabou de me mostrar
-const tarefaController = require('../controllers/tarefaController');
+const Tarefa = require('../models/tarefa');
 
-// Define as rotas (O '/' aqui vira '/tarefas' por causa do index.js)
-router.post('/', tarefaController.create);
-router.get('/', tarefaController.getAll);
+// Buscar todas as tarefas
+exports.getAll = async (req, res) => {
+  try {
+    const tarefas = await Tarefa.findAll();
+    res.status(200).json(tarefas);
+  } catch (error) {
+    res.status(500).json({ 
+      message: 'Erro ao buscar tarefas', 
+      error: error.message 
+    });
+  }
+};
 
-module.exports = router;
+// Criar uma nova tarefa
+exports.create = async (req, res) => {
+  try {
+    const novaTarefa = await Tarefa.create(req.body);
+    res.status(201).json(novaTarefa);
+  } catch (error) {
+    res.status(500).json({ 
+      message: 'Erro ao criar tarefa', 
+      error: error.message 
+    });
+  }
+};
