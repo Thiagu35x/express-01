1ca04b3e288c9969d9e30a24e94d1cbe3ba3d272
Thiago Castro <thiagocastro799@gmail.com>
2026-04-17 17:13:04 +0000

    Rotas conectadas

--- api/controllers/tarefaController.js
+++ api/controllers/tarefaController.js
@@ -1,23 +1,10 @@
-const tarefaService = require('../services/tarefaService');
+const express = require('express');
+const router = express.Router();
+// Importa o controlador que você acabou de me mostrar
+const tarefaController = require('../controllers/tarefaController');
 
-class TarefaController {
-    async create(req, res) {
-        try {
-            const tarefa = await tarefaService.create(req.body);
-            res.status(201).json(tarefa);
-        } catch (e) {
-            res.status(500).json({ error: e.message });
-        }
-    }
+// Define as rotas (O '/' aqui vira '/tarefas' por causa do index.js)
+router.post('/', tarefaController.create);
+router.get('/', tarefaController.getAll);
 
-    async getAll(req, res) {
-        try {
-            const tarefas = await tarefaService.getAll();
-            res.status(200).json(tarefas);
-        } catch (e) {
-            res.status(500).json({ error: e.message });
-        }
-    }
-}
-
-module.exports = new TarefaController();
+module.exports = router;
