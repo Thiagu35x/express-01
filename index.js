31ccdc00d519c78b8bb2f6c13e5fe26703eadfa6
Thiago Castro <thiagocastro799@gmail.com>
2026-04-17 19:35:15 +0000

    Fix: Adicionando require das rotas e salvando arquivo

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
 const express = require('express');
 const app = express();
 
-// IMPORTANTE: Esta linha liga o index.js ao seu arquivo de rotas
+// ESTA LINHA É A CHAVE: Ela importa as rotas da pasta api/routes
 const tarefaRoutes = require('./api/routes/tarefa'); 
 
 app.use(express.json());
 
-// Rota de teste para confirmar que o servidor subiu
+// Rota de teste
 app.get('/', (req, res) => res.send('Servidor Online na Vercel!'));
 
-// Ativa as rotas de tarefas no caminho /tarefas
+// Ativa as rotas
 app.use('/tarefas', tarefaRoutes);
 
 module.exports = app;
