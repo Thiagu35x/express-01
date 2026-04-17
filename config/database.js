b5245736baa85f8d2e6649b2c92f54106e3506da
Thiago Castro <thiagocastro799@gmail.com>
2026-04-17 17:32:56 +0000

    Ajuste de SSL para o banco

--- config/database.js
+++ config/database.js
@@ -2,13 +2,14 @@ const { Sequelize } = require('sequelize');
 require('dotenv').config();
 
 const sequelize = new Sequelize(process.env.DATABASE_URL, {
-    dialect: 'postgres',
-    dialectOptions: {
-        ssl: {
-            require: true,
-            rejectUnauthorized: false // Necessário para NeonDB e Vercel
-        }
+  dialect: 'postgres',
+  protocol: 'postgres',
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false // <--- Isso aqui é o segredo para a Vercel aceitar o Neon
     }
+  }
 });
 
 module.exports = sequelize;
