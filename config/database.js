1c81cc7b8d5f2d47ad5a03d30f6882e7820ac41b
Thiago Castro <thiagocastro799@gmail.com>
2026-04-17 17:51:16 +0000

    Ajuste de SSL para o banco

--- config/database.js
+++ config/database.js
@@ -7,9 +7,9 @@ const sequelize = new Sequelize(process.env.DATABASE_URL, {
   dialectOptions: {
     ssl: {
       require: true,
-      rejectUnauthorized: false // <--- ESSA LINHA resolve o Erro 500 na Vercel
+      rejectUnauthorized: false // <--- ESSA LINHA resolve o erro 500 na Vercel
     }
   }
 });
 
-module.exports = sequelize;git add .
+module.exports = sequelize;
